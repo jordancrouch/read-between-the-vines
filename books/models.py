0f42fffe4d64fb8c7fb5f8a4cb6bb2d2b43a6f4d
Jordan Crouch <107533237+jordancrouch@users.noreply.github.com>
2025-06-07 23:06:27 +0100

    Feat: add books model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Books(models.Model):
+    class Status(models.IntegerChoices):
+        DRAFT = 0, "Draft"
+        PUBLISHED = 1, "Published"
+
+    class ReadingStatus(models.TextChoices):
+        TO_BE_READ = "TBR", "To Be Read"
+        CURRENTLY_READING = "CR", "Currently Reading"
+        FINISHED_READING = "FR", "Finished Reading"
+
+    title: models.CharField = models.CharField(max_length=100, unique=True)
+    slug: models.SlugField = models.SlugField(max_length=100, unique=True)
+    content: models.TextField = models.TextField()
+    created_on: models.DateTimeField = models.DateTimeField(auto_now_add=True)
+    status: models.IntegerField = models.IntegerField(
+        choices=Status.choices, default=Status.DRAFT
+    )
+    category: models.CharField = models.CharField(
+        max_length=3,
+        choices=ReadingStatus.choices,
+        default=ReadingStatus.TO_BE_READ,
+    )
