bd8bb7f2cb35abff0341b06cf029840ba569f26b
Jordan Crouch <107533237+jordancrouch@users.noreply.github.com>
2025-06-08 19:06:32 +0100

    Feat: add list view for books that have been read

--- books/views.py
+++ books/views.py
@@ -6,5 +6,5 @@ from .models import Books
 
 # Create your views here.
 class BooksList(generic.ListView):
-    queryset = Books.objects.all()
+    queryset = Books.objects.filter(category="FR", status=1).order_by("-created_on")
     template_name = "books_list.html"
