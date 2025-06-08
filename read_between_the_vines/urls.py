fdbfc8de5d846acc9788cf8119ecab252a7d1dac
Jordan Crouch <107533237+jordancrouch@users.noreply.github.com>
2025-06-08 19:25:42 +0100

    Feat: replace ckeditor with summernote; add filters to books admin

--- read_between_the_vines/urls.py
+++ read_between_the_vines/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("summernote/", include("django_summernote.urls")),
     path("", include("books.urls"), name="books-urls"),
 ]
