c81800ea2edf0ab90d9e27cd1e312c96395dbb8c
Jordan Crouch <107533237+jordancrouch@users.noreply.github.com>
2025-06-08 09:12:09 +0100

    Feat: update books model

--- books/models.py
+++ books/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Books(models.Model):
+    class Meta:
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+
     class Status(models.IntegerChoices):
         DRAFT = 0, "Draft"
         PUBLISHED = 1, "Published"
@@ -14,8 +18,10 @@ class Books(models.Model):
 
     title: models.CharField = models.CharField(max_length=100, unique=True)
     slug: models.SlugField = models.SlugField(max_length=100, unique=True)
+    excerpt: models.TextField = models.TextField(null=True, blank=True)
     content: models.TextField = models.TextField()
     created_on: models.DateTimeField = models.DateTimeField(auto_now_add=True)
+    updated_on: models.DateTimeField = models.DateTimeField(auto_now=True)
     status: models.IntegerField = models.IntegerField(
         choices=Status.choices, default=Status.DRAFT
     )
