b5c30d9df90a6f9c695d48b281c50e92ad1a42e9
Jordan Crouch <107533237+jordancrouch@users.noreply.github.com>
2025-06-19 17:52:43 +0100

    Feat: enable serving of image files

--- read_between_the_vines/settings.py
+++ read_between_the_vines/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com"]
 
@@ -44,7 +44,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cloudinary_storage",
     "django_summernote",
+    "cloudinary",
     "home",
     "books",
 ]
