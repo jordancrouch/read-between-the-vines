6408bbf5c23456bc813a213c2c3dd47fef25b6f5
Jordan Crouch <107533237+jordancrouch@users.noreply.github.com>
2025-06-22 17:40:37 +0100

    Feat: add book tilt effect

--- static/js/site.js
+++ static/js/site.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("static js loaded");
+  const bookCovers = document.querySelectorAll(".book-cover");
+
+  const titleEffect = (e) => {
+    const book = e.currentTarget;
+    const rect = book.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    const deltaX = (mouseX - centerX) / rect.width;
+    const deltaY = (mouseY - centerY) / rect.height;
+
+    const tiltStrength = 20;
+
+    gsap.to(book.querySelector("img"), {
+      rotationX: -deltaY * tiltStrength,
+      rotationY: deltaX * tiltStrength,
+      ease: "power2.out",
+      duration: 0.3,
+    });
+  };
+
+  bookCovers.forEach((book) => {
+    book.addEventListener("mousemove", titleEffect);
+
+    book.addEventListener("mouseleave", () => {
+      gsap.to(book.querySelector("img"), {
+        rotationX: 0,
+        rotationY: 0,
+        ease: "power2.out",
+        duration: 0.3,
+      });
+    });
+  });
 });
